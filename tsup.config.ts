07f3cb89b473e196b88ba2bcce6e9ea03d7d55e1
yintingwen <yintingwen@foxmail.com>
2023-01-09 12:09:18 +0800

    尝试tsup打包

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { Options } from "tsup";
+
+export const tsup: Options = {
+    entry: [
+        'src/*.ts'
+    ],
+    format: ['cjs', 'esm'],
+    dts: true,
+    splitting: true,
+    clean: true,
+    shims: true
+}
